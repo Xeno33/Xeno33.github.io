5533198e42ff472de1717245e331961185659c90
Xeno33 <lzeno3399@gmail.com>
2016-03-21 04:49:44 -0400

    Added files via upload

---
+++ Project.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Project : MonoBehaviour {
+    public Vector3 destination;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
